f36a1a481a2b576b951dfaf492304fa662ca06d1
Zachary Brickler <zachary.brickler@mesh-systems.com>
2024-03-24 11:43:54 -0400

    fixup! Added Log file

--- Application.py
+++ Application.py
@@ -110,19 +110,19 @@ async def main():
         
             if b'MD' in motionData:
                 print("Motion activity")
-                activityLog.write("Motion activity: ", time.time())
+                activityLog.write("Motion activity: " + str(time.time()) + "\n")
                 await asyncio.gather(ble1.send_loop(), acknowledge(ble1))
                 motionData = b''
 
             if b'PLACEHOLDER' in windowData:
                 print("Window activity")
-                activityLog.write("Window activity: ", time.time())
+                activityLog.write("Window activity: " + str(time.time()) + "\n")
                 await asyncio.gather(ble2.send_loop(), acknowledge(ble2))
                 windowData = b''
 
             if b'DO' in doorData:
                 print("Door activity")
-                activityLog.write("Door activity: ", time.time())
+                activityLog.write("Door activity: " + str(time.time()) + "\n")
                 await asyncio.gather(ble3.send_loop(), acknowledge(ble3))
                 doorData = b''
 
