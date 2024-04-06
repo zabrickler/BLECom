9f924a4112dbc2f7b37a6fe7e4ce7f7e56ff5e8e
Zachary Brickler <zachary.brickler@mesh-systems.com>
2024-04-05 21:29:51 -0400

    Updated scanning, arm/disarm states, and attempted write_enabled no attribute bug fix

--- Application.py
+++ Application.py
@@ -66,9 +66,7 @@ async def disarmDoor(ble: BLE_interface):
     while not(b'A' in doorData):
         await asyncio.sleep(3.0)
         print("Sending disarm")
-        #if(b'A' in doorData):
         ble.queue_send(b'DSD')
-        #ble.queue_send(b'RSD')
     await ble.disconnect()
 
 async def disarmWindow(ble: BLE_interface):
@@ -77,9 +75,7 @@ async def disarmWindow(ble: BLE_interface):
     while not(b'A' in windowData):
         await asyncio.sleep(3.0)
         print("Sending disarm")
-        #if(b'A' in windowData):
         ble.queue_send(b'PLACEHOLDER')
-        #ble.queue_send(b'PLACEHOLDER')
     await ble.disconnect()
 
 async def armMotion(ble: BLE_interface):
@@ -112,12 +108,11 @@ async def connection(device, rwuuid, adapter, suuid):
         ble.set_receiver(motion_callback)
     elif(device == "44:B7:D0:2D:6A:B3"):
         ble.set_receiver(window_callback)
-    else:
+    elif(device == "44:B7:D0:2C:D3:27"):
         ble.set_receiver(door_callback)
     try:
         await ble.connect(device, "public", 10.0)
         await ble.setup_chars(rwuuid, rwuuid, "rw")
-        #await asyncio.gather(ble.send_loop(), hello_sender(ble))
     except Exception as e:
         print("An error occurred:", e)
     
@@ -132,7 +127,9 @@ async def main():
     doorData = b''
     activityLog = open("activityLog.txt", "a")
     PiArmedState = True
-    DevicesArmedState = True
+    MotionArmedState = True
+    WindowArmedState = True
+    DoorArmedState = True
     notifyURL = 'https://maker.ifttt.com/trigger/Motion_detected/json/with/key/drkDV7BLk5F_sqrYTDTwbW'
     dbURL = 'https://home-security-90ecb2cf8b83.herokuapp.com/securitystatus'
 
@@ -161,21 +158,23 @@ async def main():
                 ble1 = await connection(device1, uuid, ADAPTER, SERVICE_UUID)
                 print(ble1)
             except KeyError:
-                try:
-                    print(devices[device2])
-                    global ble2
-                    windowCheckInTime = time.time()
-                    ble2 = await connection(device2, uuid, ADAPTER, SERVICE_UUID)
-                    print(ble2)
-                except KeyError:
-                    try:
-                        print(devices[device3])
-                        global ble3
-                        doorCheckInTime = time.time()
-                        ble3 = await connection(device3, uuid, ADAPTER, SERVICE_UUID)
-                        print(ble3)
-                    except KeyError:
-                        print("No device found")
+                print("Motion not found")
+            try:
+                print(devices[device2])
+                global ble2
+                windowCheckInTime = time.time()
+                ble2 = await connection(device2, uuid, ADAPTER, SERVICE_UUID)
+                print(ble2)
+            except KeyError:
+                print("Window not found")
+            try:
+                print(devices[device3])
+                global ble3
+                doorCheckInTime = time.time()
+                ble3 = await connection(device3, uuid, ADAPTER, SERVICE_UUID)
+                print(ble3)
+            except KeyError:
+                print("Door not found")
 
             response = requests.get(dbURL)
             data = response.json()
@@ -185,7 +184,8 @@ async def main():
             elif(ArmedStatus == 'Disarm'):
                 PiArmedState = False
 
-            if(PiArmedState and DevicesArmedState):        
+            #Armed and awaiting messages
+            if(PiArmedState and (MotionArmedState or WindowArmedState or DoorArmedState)):        
                 if b'MD' in motionData:
                     print("Motion activity")
                     theTime = time.localtime()
@@ -228,38 +228,69 @@ async def main():
                     doorData = b''
                     ble3 = None
 
-            if(not(PiArmedState) and DevicesArmedState):
+            #Disarmed
+            if(not(PiArmedState)):
                 if not(ble1 == None):
-                    await asyncio.gather(ble1.send_loop(), disarmMotion(ble1))
+                    try:
+                        await asyncio.gather(ble1.send_loop(), disarmMotion(ble1))
+                        MotionArmedState = False
+                    except Exception as e:
+                        print("An error occurred", e)
+                        ble1.disconnect()
                     motionData = b''
                     ble1 = None
 
                 if not(ble2 == None):
-                    await asyncio.gather(ble2.send_loop(), disarmWindow(ble2))
+                    try:
+                        await asyncio.gather(ble2.send_loop(), disarmWindow(ble2))
+                        WindowArmedState = False
+                    except Exception as e:
+                        print("An error occurred", e)
+                        ble2.disconnect()
                     windowData = b''
                     ble2 = None
+
                 print("Do we get here")
                     
                 if not(ble3 == None):
-                    await asyncio.gather(ble3.send_loop(), disarmDoor(ble3))
+                    try:
+                        await asyncio.gather(ble3.send_loop(), disarmDoor(ble3))
+                        DoorArmedState = False
+                    except Exception as e:
+                        print("An error occurred", e)
+                        ble3.disconnect()
+                    print(ble3) #This is to see the 
                     doorData = b''
                     ble3 = None
-                #DevicesArmedState = False
-                #THIS NEEDS TO BE IMPLEMENTED WHEN ALL ARE DISARMED
 
-            if(PiArmedState and not(DevicesArmedState)):
+            #Rearming
+            if(PiArmedState and (not(MotionArmedState) or not(WindowArmedState) or not(DoorArmedState))):
                 if not(ble1 == None):
-                    await asyncio.gather(ble1.send_loop(), armMotion(ble1))
+                    try:
+                        await asyncio.gather(ble1.send_loop(), armMotion(ble1))
+                        MotionArmedState = True
+                    except Exception as e:
+                        print("An error occurred", e)
+                        ble1.disconnect()
                     motionData = b''
                     ble1 = None
                 
                 if not(ble2 == None):
-                    await asyncio.gather(ble2.send_loop(), armWindow(ble2))
+                    try:
+                        await asyncio.gather(ble2.send_loop(), armWindow(ble2))
+                        WindowArmedState = True
+                    except Exception as e:
+                        print("An error occurred", e)
+                        ble2.disconnect()
                     windowData = b''
                     ble2 = None
                 
                 if not(ble3 == None):
-                    await asyncio.gather(ble3.send_loop(), armDoor(ble3))
+                    try:
+                        await asyncio.gather(ble3.send_loop(), armDoor(ble3))
+                    except Exception as e:
+                        print("An error occurred", e)
+                        ble3.disconnect()
                     doorData = b''
                     ble3 = None
 
